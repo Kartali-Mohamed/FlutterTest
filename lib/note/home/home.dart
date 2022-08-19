6ae77fee1c9deb911a7426a12edc585b20b6dd4c
kartali mohamed <mohameekartali@gmail.com>
2022-08-19 19:17:26 +0100

    test flutter

--- lib/note/home/home.dart
+++ lib/note/home/home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
